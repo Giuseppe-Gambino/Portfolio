c29b308f78e930fd9f7b3e6590749f3615a4fa77
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2025-05-07 11:23:03 +0200

    tailwind, daisyui, gsap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Portfolio';
+
+  ngAfterViewInit() {
+    gsap.to('.box', { rotation: 360, duration: 2 });
+  }
 }
