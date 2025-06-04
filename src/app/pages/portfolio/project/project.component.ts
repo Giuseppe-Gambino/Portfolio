8a3fa5d0b9be7cf482cbc9c8fd68c58e073527c6
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2025-06-04 17:23:33 +0200

    animation cascade

--- src/app/pages/portfolio/project/project.component.ts
+++ src/app/pages/portfolio/project/project.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavAnimationService } from '../../../mainComponent/navbar/nav-animation.service';
 import { Project, PROJECTS } from '../../../data/projects';
 import { ActivatedRoute } from '@angular/router';
+import { gsap } from 'gsap/gsap-core';
 
 @Component({
   selector: 'app-project',
@@ -18,8 +19,20 @@ export class ProjectComponent {
 
   project!: Project;
 
+  ngAfterViewInit() {
+    const tl = gsap.timeline({ defaults: { ease: 'power4.out', duration: 1 } });
+
+    tl.from('.banner', { y: -100, opacity: 0 })
+      .addLabel('startContent', '-=0.4')
+      .to('.sec1', { opacity: 1 }, 'startContent')
+      .to('.sec2', { opacity: 1 }, 'startContent+=0.3')
+      .to('.sec3', { opacity: 1 }, 'startContent+=0.6');
+  }
+
   ngOnInit(): void {
-    this.navAn.show();
+    setTimeout(() => {
+      this.navAn.show();
+    }, 1000);
 
     if (this.route.snapshot.paramMap.get('id') !== null) {
       const id = parseInt(this.route.snapshot.paramMap.get('id')!);
