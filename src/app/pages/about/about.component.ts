67772e24d3996758c70f9358ebacfb5ada30e9e7
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2025-06-05 20:49:56 +0200

    about page WIP

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavAnimationService } from '../../mainComponent/navbar/nav-animation.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  constructor(private navAn: NavAnimationService) {}
 
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.navAn.show();
+    }, 500);
+  }
 }
