1647702d3c81549106e60b11884b2839ca15a33d
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2025-06-06 18:54:42 +0200

    home page divisa per component mobile/desktop

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../../shared/shared.module';
+import { HomeDesktopComponent } from './home-desktop/home-desktop.component';
+import { HomeMobileComponent } from './home-mobile/home-mobile.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HomeDesktopComponent, HomeMobileComponent],
   imports: [CommonModule, HomeRoutingModule, SharedModule],
 })
 export class HomeModule {}
