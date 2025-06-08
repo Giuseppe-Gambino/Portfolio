11c84fa4f03d4d04974a6973855b630ee99a06c5
Giuseppe-Gambino <pippo.gmb18@gmail.com>
2025-06-08 11:26:07 +0200

    background v1 + animation smooth navbar mobile

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { gsap } from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
-import { NavAnimationService } from '../../mainComponent/navbar/nav-animation.service';
 import ScrollSmoother from 'gsap/ScrollSmoother';
-import { LenisService } from '../../services/lenis.service';
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 @Component({
